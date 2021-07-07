58d814802da0d82cc8739b1678a8d9b961f8144b
JR.Ferrer.Paris <JR.Ferrer.Paris@gmail.com>
2021-07-07 08:09:07 +0000

    Updated Level3/Indicative-maps/trans-realms/TF1-biome/TF1_6.js (added 8 lines, deleted 4 lines).

--- Level3/Indicative-maps/trans-realms/TF1-biome/TF1_6.js
+++ Level3/Indicative-maps/trans-realms/TF1-biome/TF1_6.js
@@ -1,13 +1,17 @@
 /**** Start of imports. If edited, may not auto-convert in the playground. ****/
 var image = ee.Image("users/jrferrerparis/IUCN-GET/L3_IM/TF1_6");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
-// name of functional group
-
+// load module
 var slegend=require("users/jrferrerparis/IUCN-GET:simple-legend.js");
+
+// name of functional group
 var EFGname = 'TF1.6 Boreal, temperate and montane peat bogs';
+
+// create legend and title elements
 var legend=slegend.minorMajorLegend(EFGname);
 var title=slegend.titleLabel();
 
+// add everything to the map
 Map.addLayer(image, {
   bands: ['b1'],
   palette: ['red', 'yellow'],
