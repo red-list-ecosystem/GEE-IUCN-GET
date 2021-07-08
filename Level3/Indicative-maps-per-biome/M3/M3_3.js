ad4d2fa751f5f564b446d879b5e8ce136517a201
jrfep <jr.ferrer.paris@gmail.com>
2021-07-08 17:10:05 +1000

    Add scripts for functional groups

---
+++ Level3/Indicative-maps-per-biome/M3/M3_3.js
@@ -0,0 +1,21 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var EFG_IM = ee.Image("users/jrferrerparis/IUCN-GET/L3_IM/M3_3");
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
+// load module
+var slegend=require("users/jrferrerparis/IUCN-GET:simple-legend.js");
+
+// name of functional group
+var EFGname = 'M3.3 Abyssal plains';
+
+// create legend and title elements
+var legend=slegend.minorMajorLegend(EFGname);
+var title=slegend.titleLabel();
+
+// add everything to the map
+Map.addLayer(EFG_IM, {
+  bands: ['occurrence_type'],
+  palette: ['red', 'yellow'],
+  min: 1, max: 2
+}, EFGname + ' -- Indicative Map', true, 0.7);
+Map.add(title);
+Map.add(legend);
