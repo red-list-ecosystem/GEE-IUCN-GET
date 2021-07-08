94eedf2d80f32151ce47178e66d636c50dee7481
jrfep <jr.ferrer.paris@gmail.com>
2021-07-08 10:13:19 +1000

    add T2.5

---
+++ Level3/Indicative-maps/T-core-realm/T2-biome/T2_5.js
@@ -0,0 +1,21 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var image = ee.Image("users/jrferrerparis/IUCN-GET/L3_IM/T2_5");
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
+// load module
+var slegend=require("users/jrferrerparis/IUCN-GET:simple-legend.js");
+
+// name of functional group
+var EFGname = 'T2.5 Temperate pyric humid forests';
+
+// create legend and title elements
+var legend=slegend.minorMajorLegend(EFGname);
+var title=slegend.titleLabel();
+
+// add everything to the map
+Map.addLayer(image, {
+  bands: ['b1'],
+  palette: ['red', 'yellow'],
+  min: 1, max: 2
+}, 'Indicative Map', true, 0.7);
+Map.add(title);
+Map.add(legend);
