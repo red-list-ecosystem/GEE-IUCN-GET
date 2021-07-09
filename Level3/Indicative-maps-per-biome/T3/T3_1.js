629884caae9f0678b3e49d115f12abc1531c576c
jrfep <jr.ferrer.paris@gmail.com>
2021-07-09 11:04:55 +1000

    Adding scripts for T7, T1, T3 and SM1

---
+++ Level3/Indicative-maps-per-biome/T3/T3_1.js
@@ -0,0 +1,21 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var EFG_IM = ee.Image("users/jrferrerparis/IUCN-GET/L3_IM/T3_1");
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
+// load module
+var slegend=require("users/jrferrerparis/IUCN-GET:simple-legend.js");
+
+// name of functional group
+var EFGname = 'T3.1 Seasonally dry tropical shrublands';
+
+// create legend and title elements
+var legend=slegend.minorMajorLegend(EFGname);
+var title=slegend.titleLabel();
+
+// add everything to the map
+Map.addLayer(EFG_IM, {
+  bands: ['occurrence_type'],
+  palette: ['red', 'yellow'],
+  min: 1, max: 2
+}, EFGname + ' -- Indicative Map', true, 0.7);
+Map.add(title);
+Map.add(legend);
