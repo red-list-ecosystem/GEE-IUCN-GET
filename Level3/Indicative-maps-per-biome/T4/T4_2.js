0637d019a2c6176ca74ab471a607119921753288
jrfep <jr.ferrer.paris@gmail.com>
2021-07-09 18:44:10 +1000

    Update script to add ecoregions

--- Level3/Indicative-maps-per-biome/T4/T4_2.js
+++ Level3/Indicative-maps-per-biome/T4/T4_2.js
@@ -27,7 +27,7 @@ var ecoRegions = ee.FeatureCollection("RESOLVE/ECOREGIONS/2017");
 
 // x-walk of ecoregion ids
 var xwalk = ee.FeatureCollection("users/jrferrerparis/IUCN-GET/L3_IM/xwalk_teow")
-var slc = xwalk.filter(ee.Filter.eq('efg_code','EFG_code'));
+var slc = xwalk.filter(ee.Filter.eq('efg_code','T4.2'));
 // Map the function over the features.
 var xw_major = slc.filter(ee.Filter.equals('occurrence',1)).aggregate_array('eco_id');
 var xw_minor = slc.filter(ee.Filter.equals('occurrence',2)).aggregate_array('eco_id');
