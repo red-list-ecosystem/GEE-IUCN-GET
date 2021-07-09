b14f2ef90cd864a8ab345bd29fd2ac8c0c6bb01b
JR.Ferrer.Paris <JR.Ferrer.Paris@gmail.com>
2021-07-09 23:22:32 +0000

    Updated Level3/Indicative-maps-per-biome/MT2/MT2_1.js (added 12 lines).

--- Level3/Indicative-maps-per-biome/MT2/MT2_1.js
+++ Level3/Indicative-maps-per-biome/MT2/MT2_1.js
@@ -19,3 +19,15 @@ Map.addLayer(EFG_IM, {
 }, EFGname + ' -- Indicative Map', true, 0.7);
 Map.add(title);
 Map.add(legend);
+
+//
+
+//Global Shoreline Dataset
+
+var mainlands = ee.FeatureCollection('projects/sat-io/open-datasets/shoreline/mainlands');
+var big_islands = ee.FeatureCollection('projects/sat-io/open-datasets/shoreline/big_islands');
+var small_islands = ee.FeatureCollection('projects/sat-io/open-datasets/shoreline/small_islands');
+
+Map.addLayer(ee.Image().paint(mainlands,0,3), {"palette":["008000"]}, 'Shoreline Mainlands', false, 0.7);
+Map.addLayer(ee.Image().paint(big_islands,0,3), {"palette":["0000FF"]}, 'Shoreline Big Islands', false, 0.7);
+Map.addLayer(ee.Image().paint(small_islands,0,3), {"palette":["FF0000"]}, 'Shoreline Small Islands', false, 0.7);
