79f4d2fb1b93daadd0423841f5e0c2f68460a0e5
JR.Ferrer.Paris <JR.Ferrer.Paris@gmail.com>
2021-07-09 09:14:40 +0000

    Updated Level3/Indicative-maps-per-biome/T6/T6_1.js (added 11 lines).

--- Level3/Indicative-maps-per-biome/T6/T6_1.js
+++ Level3/Indicative-maps-per-biome/T6/T6_1.js
@@ -24,3 +24,14 @@ Map.add(legend);
 
 // GLIMS database
 var glims = ee.FeatureCollection("GLIMS/current");
+Map.addLayer(glims, {color:'aqua'}, 'GLIMS glaciers', false);
+
+
+// GLIMS database
+var snow1k = ee.Image('users/murrnick/red-list-eco/globalTypology/snowOccurrence_1k_v1')
+var snow30m = ee.Image('users/murrnick/red-list-eco/globalTypology/snowOccurrence_30m_v1')
+
+var plasma = ["0d0887", "3d049b", "6903a5", "8d0fa1", "ae2891", "cb4679", "df6363", "f0844c", "faa638", "fbcc27", "f0f921"];
+
+Map.addLayer(snow1k.updateMask(snow1k.gt(70)), {palette: plasma, min:70, max:100},'Murray -- snow 1k', false)
+Map.addLayer(snow30m.updateMask(snow30m.gt(50)), {palette: plasma, min:50, max:100},'Murray -- snow 30m',false)
