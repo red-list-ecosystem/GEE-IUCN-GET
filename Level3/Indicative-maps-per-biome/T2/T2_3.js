076bb5f09c83b8a1dbff74e78026b5c3a31ebe4d
JR.Ferrer.Paris <JR.Ferrer.Paris@gmail.com>
2021-07-10 05:45:22 +0000

    Updated Level3/Indicative-maps-per-biome/T2/T2_3.js (added 25 lines).

--- Level3/Indicative-maps-per-biome/T2/T2_3.js
+++ Level3/Indicative-maps-per-biome/T2/T2_3.js
@@ -19,3 +19,28 @@ Map.addLayer(EFG_IM, {
 }, EFGname + ' -- Indicative Map', true, 0.7);
 Map.add(title);
 Map.add(legend);
+
+
+//
+
+
+// OpenLandMap Potential distribution of biomes
+// https://developers.google.com/earth-engine/datasets/catalog/OpenLandMap_PNV_PNV_BIOME-TYPE_BIOME00K_C_v01
+
+var dataset = ee.Image("OpenLandMap/PNV/PNV_BIOME-TYPE_BIOME00K_C/v01");
+
+var visualization = {
+  bands: ['biome_type'],
+  min: 1.0,
+  max: 32.0,
+  palette: [
+    "1c5510","659208","ae7d20","000065","bbcb35","009a18",
+    "caffca","55eb49","65b2ff","0020ca","8ea228","ff9adf",
+    "baff35","ffba9a","ffba35","f7ffca","e7e718","798649",
+    "65ff9a","d29e96",
+  ]
+};
+
+Map.centerObject(dataset);
+
+Map.addLayer(dataset, visualization, "Potential distribution of biomes", false, 0.5);
