75f11bb7401da4913bd1c6eca51055d209ad3616
jrfep <jr.ferrer.paris@gmail.com>
2021-07-13 17:56:58 +1000

    Update subterranean realm using imageCollection

--- Level3/Indicative-maps-per-biome/S1/S1_2.js
+++ Level3/Indicative-maps-per-biome/S1/S1_2.js
@@ -1,11 +1,12 @@
 /**** Start of imports. If edited, may not auto-convert in the playground. ****/
-var EFG_IM = ee.Image("users/jrferrerparis/IUCN-GET/L3_IM/S1_2");
+var indMaps = ee.ImageCollection('users/jrferrerparis/IUCN-GET/L3_IndMaps');
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 // load module
 var slegend=require("users/jrferrerparis/IUCN-GET:simple-legend.js");
 
 // name of functional group
 var EFGname = 'S1.2 Endolithic systems';
+var EFG_IM = indMaps.filter(ee.Filter.equals("title", EFGname))
 
 // create legend and title elements
 var legend=slegend.minorMajorLegend(EFGname);
--- Level3/Indicative-maps-per-biome/SM1/SM1_3.js
+++ Level3/Indicative-maps-per-biome/SM1/SM1_3.js
@@ -1,11 +1,12 @@
 /**** Start of imports. If edited, may not auto-convert in the playground. ****/
-var EFG_IM = ee.Image("users/jrferrerparis/IUCN-GET/L3_IM/SM1_3");
+var indMaps = ee.ImageCollection('users/jrferrerparis/IUCN-GET/L3_IndMaps');
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 // load module
 var slegend=require("users/jrferrerparis/IUCN-GET:simple-legend.js");
 
 // name of functional group
 var EFGname = 'SM1.3 Sea caves';
+var EFG_IM = indMaps.filter(ee.Filter.equals("title", EFGname))
 
 // create legend and title elements
 var legend=slegend.minorMajorLegend(EFGname);
