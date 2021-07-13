d57693e519d14cacc7616636454f55e2f6458183
JR.Ferrer.Paris <JR.Ferrer.Paris@gmail.com>
2021-07-13 04:58:11 +0000

    Updated Level3/all-indicative-maps.js (added 20 lines, deleted 12 lines).

--- Level3/all-indicative-maps.js
+++ Level3/all-indicative-maps.js
@@ -1,14 +1,17 @@
-var indMaps = ee.ImageCollection('users/jrferrerparis/IUCN-GET/L3_IndMaps');
-print(indMaps);
+// Example of spatial queries for the indicative maps
 
-// Get the number of images.
+// load the image collection:
+var indMaps = ee.ImageCollection('users/jrferrerparis/IUCN-GET/L3_IndMaps');
+print('Indicative maps of the IUCN GET:',indMaps);
+
+// Get the number of functional groups (EFGs).
 var count = indMaps.size();
-print('Count: ', count);
+print('Number of EFGs in the collection: ', count);
 // Get list of names
 var names = indMaps.aggregate_array('title');
-print('All names: ', names);
+print('List of EFG names: ', names);
 
-// example how to extract one EFG:
+// example how to extract and visualise one EFG:
 var EFGshortname = 'M1.6 Subtidal rocky reefs';
 var EFG = indMaps.filter(ee.Filter.equals('title', EFGshortname));
 
@@ -19,20 +22,21 @@ Map.addLayer(EFG, {
 }, EFGshortname + ' -- Indicative Map', true, 0.7);
 
 
-
 // example query at point location
+// define location:
 var geometry = ee.Geometry.Point([-69.679, 9.479]);
 
 var reductionTable = indMaps.map(function(img) {
   var stats = ee.Dictionary(img.reduceRegion(
       {reducer: ee.Reducer.first(), geometry: geometry, scale: 250}));
-  //var row = ee.Number(stats.get('occurrence_type'));
-  //return ee.Feature(null, {'occ':row});
   return ee.Feature(null, stats);
 });
-// query a a single point
+// filter not empty results
 var filteredTable=reductionTable.filter(ee.Filter.notNull(['occurrence_type']));
-print('EFGs at point location:', filteredTable);
+
+var count=filteredTable.size();
+print('How many EFGs at this location: ', count);
+print('List of EFGs at this location:', filteredTable);
 
 // -- Create a geometry of Caracas (Venezuela's capital)
 var geomCaracas = ee.Geometry.Rectangle([-67.062383, 10.558489, -66.667078, 10.364908]);
@@ -46,4 +50,8 @@ var reductionTable = indMaps.map(function(img) {
 });
 // this does not work
 var filteredTable=reductionTable.filter(ee.Filter.notNull(['occurrence_type']));
-print('EFGs at selected region:', filteredTable);
+
+var count=filteredTable.size();
+print('How many EFGs in this region: ', count);
+print('List of EFGs in this region:', filteredTable);
+
