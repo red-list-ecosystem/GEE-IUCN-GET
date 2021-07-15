bae39748deb8a3df0d5310c2d8ba457bea252419
JR.Ferrer.Paris <JR.Ferrer.Paris@gmail.com>
2021-07-15 07:04:17 +0000

    Updated Level3/Indicative-maps-per-biome/T7/T7_4.js (added 24 lines, deleted 1 line).

--- Level3/Indicative-maps-per-biome/T7/T7_4.js
+++ Level3/Indicative-maps-per-biome/T7/T7_4.js
@@ -1,5 +1,5 @@
 /**** Start of imports. If edited, may not auto-convert in the playground. ****/
-var indMaps = ee.ImageCollection('users/jrferrerparis/IUCN-GET/L3_IndMaps');
+var indMaps = ee.ImageCollection("users/jrferrerparis/IUCN-GET/L3_IndMaps");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 // load module
 var slegend=require("users/jrferrerparis/IUCN-GET:simple-legend.js");
@@ -20,3 +20,26 @@ Map.addLayer(EFG_IM, {
 }, EFGname + ' -- Indicative Map', true, 0.7);
 Map.add(title);
 Map.add(legend);
+
+// alternative data sources
+
+// Tsinghua FROM-GLC year of change to impervious surface
+var dataset = ee.Image("Tsinghua/FROM-GLC/GAIA/v10");
+
+var visualization = {
+  bands: ['change_year_index'],
+  min: 0.0,
+  max: 34.0,
+  palette: [
+    "014352","1A492C","071EC4","B5CA36","729EAC","8EA5DE",
+    "818991","62A3C3","CCF4FE","74F0B9","32BC55","C72144",
+    "56613B","C14683","C31C25","5F6253","11BF85","A61B26",
+    "99FBC5","188AAA","C2D7F1","B7D9D8","856F96","109C6B",
+    "2DE3F4","9A777D","151796","C033D8","510037","640C21",
+    "31A191","223AB0","B692AC","2DE3F4",
+  ]
+};
+
+Map.setCenter(-37.62, 25.8, 2);
+
+Map.addLayer(dataset, visualization, "FROM-GLC Change year index",false,0.5);
