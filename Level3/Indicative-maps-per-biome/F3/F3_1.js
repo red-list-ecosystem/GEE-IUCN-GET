2cb37d1ba6aadcf148951dc3b54181aeb510e3e5
JR.Ferrer.Paris <JR.Ferrer.Paris@gmail.com>
2021-07-16 11:29:54 +0000

    Updated Level3/Indicative-maps-per-biome/F3/F3_1.js (added 2 lines, deleted 2 lines).

--- Level3/Indicative-maps-per-biome/F3/F3_1.js
+++ Level3/Indicative-maps-per-biome/F3/F3_1.js
@@ -1,12 +1,12 @@
 /**** Start of imports. If edited, may not auto-convert in the playground. ****/
-var indMaps = ee.ImageCollection('users/jrferrerparis/IUCN-GET/L3_IndMaps');
+var indMaps = ee.ImageCollection("users/jrferrerparis/IUCN-GET/L3_IndMaps");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 // load module
 var slegend=require("users/jrferrerparis/IUCN-GET:simple-legend.js");
 
 // name of functional group
 var EFGname = 'F3.1 Large reservoirs';
-var EFG_IM = indMaps.filter(ee.Filter.equals("title", EFGname))
+var EFG_IM = indMaps.filter(ee.Filter.equals("title", EFGname));
 
 // create legend and title elements
 var legend=slegend.minorMajorLegend(EFGname);
