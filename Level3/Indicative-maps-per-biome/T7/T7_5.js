81b610820cf55770515ff30d86b27b23e2b11ada
jrfep <jr.ferrer.paris@gmail.com>
2021-07-29 15:52:02 +1000

    compare with EUNIS grasslands

--- Level3/Indicative-maps-per-biome/T7/T7_5.js
+++ Level3/Indicative-maps-per-biome/T7/T7_5.js
@@ -20,3 +20,26 @@ Map.addLayer(EFG_IM, {
 }, EFGname + ' -- Indicative Map', true, 0.7);
 Map.add(title);
 Map.add(legend);
+
+
+// EUNIS map from https://www.eea.europa.eu/data-and-maps/data/ecosystem-types-of-europe-1
+//
+var EUNIS = ee.Image("users/jrferrerparis/thirdparty/EUNIS");
+
+var dict = {
+  "values": [23,24,25,26,27,28,29]
+  "names": [
+    "E1 Dry grasslands", // 23
+    "E2 Mesic grasslands", // 24
+    "E3 Seasonally wet and wet grasslands", // 25
+    "E4 Alpine and subalpine grasslands", // 26
+    "",
+    "E6 Inland salt steppes", // 28
+    "E7 Sparsely wooded grasslands" // 29
+  ],
+  "colors": [
+    "#7FC97F", "#BEAED4", "#FDC086", "#FFFF99", "#999999", "#386CB0", "#F0027F"
+  ]};
+
+var EUNIS_grasses = EUNIS.updateMask(EUNIS.gt(29).and(EUNIS.lt(23)));
+Map.addLayer(EUNIS_grasses, {min:23, max:29, palette:dict['colors']}, 'EUNIS grasslands',false,0.5)
