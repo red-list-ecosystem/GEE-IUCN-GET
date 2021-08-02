71f95f1cc6205a2b7aa47a2835b616b41a57fbc1
JR.Ferrer.Paris <JR.Ferrer.Paris@gmail.com>
2021-08-02 22:42:47 +0000

    Updated Level3/Indicative-maps-per-biome/F3/F3_5.js (added 14 lines, deleted 5 lines).

--- Level3/Indicative-maps-per-biome/F3/F3_5.js
+++ Level3/Indicative-maps-per-biome/F3/F3_5.js
@@ -1,5 +1,5 @@
 /**** Start of imports. If edited, may not auto-convert in the playground. ****/
-var indMaps = ee.ImageCollection('users/jrferrerparis/IUCN-GET/L3_IndMaps');
+var indMaps = ee.ImageCollection("users/jrferrerparis/IUCN-GET/L3_IndMaps");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 // load module
 var slegend=require("users/jrferrerparis/IUCN-GET:simple-legend.js");
@@ -23,8 +23,17 @@ Map.add(legend);
 
 //
 // Global irrigation areas
+// Deepak Nagaraj, Eleanor Proust, Alberto Todeschini, Maria Cristina Rulli, Paolo D'Odorico,
+//A new dataset of global irrigation areas from 2001 to 2015, Advances in Water Resources,
+//Volume 152,2021,103910,ISSN 0309-1708,https://doi.org/10.1016/j.advwatres.2021.103910.
+
 var irrigation_maps = ee.ImageCollection("users/deepakna/global_irrigation_maps");
+var irrigation_2010 = ee.Image(irrigation_maps.filter(ee.Filter.date('2001-01-01','2001-12-31')).first());
+var irrigation_map = irrigation_2010.updateMask(irrigation_2010.gt(0));
+
 // +-> for individual years
-var highly_irrigated_areas_2001 = ee.Image("users/deepakna/global_irrigation_maps/2001")
-  .expression("b(0) == 2 ? 1 : 0");
-Map.addLayer(highly_irrigated_areas_2001.updateMask(highly_irrigated_areas_2001.neq(0),true,0.5)
+Map.addLayer(irrigation_map, {
+  palette: ['#24b297', '#1482c7'],
+  min: 1, max: 2
+}, 'Density of irrigation', true, 0.5);
+print(irrigation_maps);
