b9c799e821cbdb7fbfbe9119de72b30e6fd800ab
JR.Ferrer.Paris <JR.Ferrer.Paris@gmail.com>
2021-11-26 07:01:29 +0000

    Updated Level3/Indicative-maps-per-biome/T4/T4_3.js (added 10 lines, deleted 2 lines).

--- Level3/Indicative-maps-per-biome/T4/T4_3.js
+++ Level3/Indicative-maps-per-biome/T4/T4_3.js
@@ -1,5 +1,6 @@
 /**** Start of imports. If edited, may not auto-convert in the playground. ****/
-var indMaps = ee.ImageCollection('users/jrferrerparis/IUCN-GET/L3_IndMaps');
+var indMaps = ee.ImageCollection("users/jrferrerparis/IUCN-GET/L3_IndMaps"),
+    newMap = ee.Image("users/jrferrerparis/IUCN-GET/L3_WM_nwt/T4_3");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 // load module
 var slegend=require("users/jrferrerparis/IUCN-GET:simple-legend.js");
@@ -17,6 +18,13 @@ Map.addLayer(EFG_IM, {
   bands: ['occurrence_type'],
   palette: ['red', 'yellow'],
   min: 1, max: 2
-}, EFGname + ' -- Indicative Map', true, 0.7);
+}, EFGname + ' -- Indicative Map (old version)', false, 0.7);
+
+Map.addLayer(newMap, {
+  bands: ['occurrence_type'],
+  palette: ['red', 'yellow'],
+  min: 1, max: 2
+}, EFGname + ' -- Indicative Map (updated)', true, 0.7);
+
 Map.add(title);
 Map.add(legend);
