934c4b21d93b5d74adb2021f7975a075349990fa
JR.Ferrer.Paris <JR.Ferrer.Paris@gmail.com>
2021-11-26 01:30:24 +0000

    Updated Level3/Indicative-maps-per-biome/T1/T1_3.js (added 9 lines, deleted 8 lines).

--- Level3/Indicative-maps-per-biome/T1/T1_3.js
+++ Level3/Indicative-maps-per-biome/T1/T1_3.js
@@ -1,7 +1,8 @@
 /**** Start of imports. If edited, may not auto-convert in the playground. ****/
 var indMaps = ee.ImageCollection("users/jrferrerparis/IUCN-GET/L3_IndMaps"),
     image = ee.Image("users/jrferrerparis/IUCN-GET/L3_WM_nwt/T1_3"),
-    image2 = ee.Image("projects/agile-tangent-319108/assets/tcf_ensemble_mnv16_2001_2018");
+    tcf_2018 = ee.Image("projects/agile-tangent-319108/assets/tcf_ensemble_mnv16_2001_2018"),
+    geometry = /* color: #d63000 */ee.Geometry.Point([-98.37966394963776, 20.477334831716146]);
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 // load module
 var slegend=require("users/jrferrerparis/IUCN-GET:simple-legend.js");
@@ -19,12 +20,12 @@ Map.addLayer(EFG_IM, {
   bands: ['occurrence_type'],
   palette: ['red', 'yellow'],
   min: 1, max: 2
-}, EFGname + ' -- Indicative Map', true, 0.7);
+}, EFGname + ' -- Indicative Map', false, 0.7);
 Map.add(title);
 Map.add(legend);
 
 // https://samapriya.github.io/awesome-gee-community-datasets/projects/gcc/
-// Global 1-km Cloud Cover :
+// Global 1-km Cloud Cover (2016):
 var cf_pred = ee.Image("projects/sat-io/open-datasets/gcc/MODCF_CloudForestPrediction");
 var msked_pred = cf_pred.updateMask(cf_pred.gte(0.0001));
 
@@ -32,7 +33,7 @@ Map.addLayer(msked_pred, {
   bands: ['b1'],
   palette: ['white', 'purple'],
   min: 0.0001, max: 0.00077
-}, 'Cloud Forest prediction', false, 0.7);
+}, 'Cloud Forest prediction 2016', false, 0.7);
 
 // new version 
 Map.addLayer(image, {
@@ -42,10 +43,10 @@ Map.addLayer(image, {
 }, EFGname + ' -- Indicative Map (new version)', false, 0.7);
 
 
-var msked_tcf = image2.updateMask(image2.gte(0));
+var msked_tcf = tcf_2018.updateMask(tcf_2018.gte(0));
 
 Map.addLayer(msked_tcf, {
   bands: ['b1'],
-  palette: ['white', 'purple'],
-  min: 0, max: 100
-}, 'Cloud Forest prediction', true, 0.7);
+  palette: ['cyan', 'purple'],
+  min: 0, max: 50
+}, 'Cloud Forest prediction 2021', true, 1.0);
