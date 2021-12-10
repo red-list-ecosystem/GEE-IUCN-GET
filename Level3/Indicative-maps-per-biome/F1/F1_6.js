87712f7e832b9dcb6ffb0d212d82d6026ac16a4c
JR.Ferrer.Paris <JR.Ferrer.Paris@gmail.com>
2021-12-10 06:10:02 +0000

    Updated Level3/Indicative-maps-per-biome/F1/F1_6.js (added 29 lines, deleted 2 lines).

--- Level3/Indicative-maps-per-biome/F1/F1_6.js
+++ Level3/Indicative-maps-per-biome/F1/F1_6.js
@@ -1,12 +1,12 @@
 /**** Start of imports. If edited, may not auto-convert in the playground. ****/
-var indMaps = ee.ImageCollection('users/jrferrerparis/IUCN-GET/L3_IndMaps');
+var indMaps = ee.ImageCollection("users/jrferrerparis/IUCN-GET/L3_IndMaps");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 // load module
 var slegend=require("users/jrferrerparis/IUCN-GET:simple-legend.js");
 
 // name of functional group
 var EFGname = 'F1.6 Episodic arid rivers';
-var EFG_IM = indMaps.filter(ee.Filter.equals("title", EFGname))
+var EFG_IM = indMaps.filter(ee.Filter.equals("title", EFGname));
 
 // create legend and title elements
 var legend=slegend.minorMajorLegend(EFGname);
@@ -20,3 +20,30 @@ Map.addLayer(EFG_IM, {
 }, EFGname + ' -- Indicative Map', true, 0.7);
 Map.add(title);
 Map.add(legend);
+
+// Need to adapt this for freshwater ecoregions:
+
+// TEOW / RESOLVE 2017
+var ecoRegions = ee.FeatureCollection("RESOLVE/ECOREGIONS/2017");
+
+// x-walk of ecoregion ids
+var xwalk = ee.FeatureCollection("users/jrferrerparis/IUCN-GET/xwalk_teow")
+var slc = xwalk.filter(ee.Filter.eq('efg_code','T4.2'));
+// Map the function over the features.
+var xw_major = slc.filter(ee.Filter.equals('occurrence',1)).aggregate_array('eco_id');
+var xw_minor = slc.filter(ee.Filter.equals('occurrence',2)).aggregate_array('eco_id');
+
+// Check contributors to the xwalk and map versions
+var xw_ver = slc.distinct(['contributors','map_code','map_version']).select(['contributors','map_code','map_version']);
+print('Cross-walk contributors and versions:')
+print(xw_ver);
+// need to get a efficient way to select the latest version:
+// print(xw_ver.aggregate_array('map_code'));
+
+// filter ecoregions by the list of eco_ids
+var teow_major = ecoRegions.filter(ee.Filter.inList('ECO_ID', xw_major));
+var teow_minor = ecoRegions.filter(ee.Filter.inList('ECO_ID', xw_minor));
+
+Map.addLayer(teow_major, {color: 'red', width: 0}, 'Ecoregions with major occurrences', false, 0.5);
+Map.addLayer(teow_minor, {color: 'yellow', width: 0}, 'Ecoregions with minor occurrences', false, 0.5);
+
